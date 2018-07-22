fb02bb40e9f5fde206620fec915ae00393207e53
vetsinen <vetsinen@gmail.com>
2018-07-22 03:53:19 +0300

    it outputs rezults

--- texts.py
+++ texts.py
@@ -1,12 +1,28 @@
-t = ['SQLAthanor: Serialization / Deserialization for SQLAlchemy ORM And now for something completely different',
-     'Framework to do an application a schedule for a University',
-     'A nice collection of often useful awesome Python frameworks and libraries',
-     'How to use PyDrive to upload files to Google Drive through python', 'Ranking News Bias with Python',
-     'I`ve made a clock using pyglet, but I want to change the window bg color every second.Why is the code I’ve used make the bg flicker and not go from white to black all the way',
-     'Sending and pulling live data from server.Dreams In Text - Text Based Adventure',
-     'I developed a package which will notify your github activity and it is working on mac windows and linux platform.',
-     'Getting the active(foreground) window title on windows 10 using python',
-     'Chameleon:An HTML / XML template engine for Python',
-     'SubstanceDesigner now allows you to script its behavior using Python', 'Fuction and Home key in pyautogui',
-     'How to install win32 on Windows 10', 'Python has brought computer programming to a vast new audience',
-     'Exceptive: A Library Making Exception Handling More Programmatic and Debuggable']
+ent = ['SQLAthanor: Serialization / Deserialization for SQLAlchemy ORM And now for something completely different',
+       'Framework to do an application a schedule for a University',
+       'A nice collection of often useful awesome Python frameworks and libraries',
+       'How to use PyDrive to upload files to Google Drive through python', 'Ranking News Bias with Python',
+       'I`ve made a clock using pyglet, but I want to change the window bg color every second.Why is the code I’ve used make the bg flicker and not go from white to black all the way',
+       'Sending and pulling live data from server.Dreams In Text - Text Based Adventure',
+       'I developed a package which will notify your github activity and it is working on mac windows and linux platform.',
+       'Getting the active(foreground) window title on windows 10 using python',
+       'Chameleon:An HTML / XML template engine for Python',
+       'SubstanceDesigner now allows you to script its behavior using Python', 'Fuction and Home key in pyautogui',
+       'How to install win32 on Windows 10', 'Python has brought computer programming to a vast new audience',
+       'Exceptive: A Library Making Exception Handling More Programmatic and Debuggable']
+
+news = [
+        'Drake`s latest No. 1 hit In My Feelings shot to the top of the charts thanks to a viral dance challenge that had little to do with the rapper himself.',
+        'Teach and learn by playing! Showcase your culture and your country in non-verbal forms: dances, videos, pictures, traditional objects and many more! Challenge your communication skills in a group of young English learners and become a child yourself once again! Leave memories of bright international cooperation!',
+        'The festival, founded by Jay-Z in 2012, takes place on Philadelphia`s streets. This week, Mayor Jim Kenney`s office announced plans to move the event — sparking fury from the rapper.',
+        'The European Court of Human Rights ruling — and the release of new music from the activist and punk collective — comes two days after members of the group interrupted the World Cup final game.',
+        'Goal of the project is to present participant’s culture and encourage young people to meet foreigners. Volunteers are going to teach basics of English and arise the interest in social issues and other cultures. Form of workshops should be adapted to age and skills of youth. Trainee should prepare engaging presentations, games and other additional activities.',
+        'A few hours earlier, the authorities had cut short Kraviz’s headline show at a nearby festival, claiming – incorrectly – that it was overrunning. So, this otherworldly afterparty feels subversive. It is being livestreamed on Facebook, which is banned in China, along with most western social media. ',
+        'Our internship program offers a select number of university students (and university graduates) the opportunity to gain exposure to a thriving law practice. In order to sharpen your legal skills, you will work alongside reputable and talented lawyers and experience an inclusive culture that encourages contribution and helps you to start fulfilling your potential.'
+    ]
+rut = [
+    'В идеале у аутсорсинговой компании есть несколько крупных долгосрочных проектов (клиентов) с хорошими рейтами. Проект растет в медленном прогнозируемом темпе, поэтому расширение команды и поиск новых сотрудников не составляет труда. Но такие проекты существуют где-то в идеальном мире. Реальность же такова, что большинство проектов краткосрочны, внедряются специфические технологии, рейты у разработчиков низкие, клиент полностью управляет командой (это больше похоже на аутстаффинг, а не аутсорсинг), цена за проект фиксирована и вам часто приходится бороться с клиентом, доказывая, что необходимо увеличить количество часов',
+    'Геймдев хакатон на сутки, сразу после конференции IGDO Conference от Indie Game Developers Odessa Локация: Chapps Space — креативное пространство в центре Одессы от digital агентства Chapps. Хакатон — это когда приходишь, придумываешь и реализуешь идею за короткий срок. Можно идти со своей идеей, можно придумать на месте. Можно присоединиться к понравившейся команде. Хакатон — это драйв, командная работа, соревнование, знакомства, новый опыт, шанс попробовать свои силы в одном флаконе',
+    ' конкурс среди команд будущих, возможно уже действующих разработчиков из 2-3 человек, а также всех желающих реализующих СВОЮ идею. Цель хакатона — создание рабочего приложения или прототипа в рамках темы, определенной несколькими критериями. Тематика данного хакатона — «Кампус будущего», а детальные критерии будут объявлены непосредственно на хакатоне, чтобы никто не мог подготовиться к конкурсу заранее, и все команды находились в равных стартовых условиях.',
+    'CoE, по сути, влияет на стратегическое развитие компании. Такие «центры» — это не просто R&D, это R&D + бизнес. Ваша наработанная (или нарабатываемая) экспертиза систематизирована, у вас есть команда сейлов, которая работает в тесной связи с R&D для того, чтобы предлагать потенциальному клиенту уникальную экспертизу активнее, а не просто ждать запроса от потенциального клиента и думать, чем занять скучающих на бенче сотрудников. Более того, благодаря СоЕ у компании появляется возможность охватить даже тех клиентов, которые даже не думали о каких- либо новых проектах.'
+]
