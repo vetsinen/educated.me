a5765afc6448c83e19ea707c8b0a204c09d59337
vetsinen <vetsinen@gmail.com>
2018-09-18 18:04:48 +0300

    way to mongo

--- flaskapp.py
+++ flaskapp.py
@@ -9,17 +9,14 @@ CORS(app)
 
 hello = '42'
 
-db = pickledb.load('dated-events.db', False)
+# db = pickledb.load('dated-events.db', False)
 import datetime
 nowstr = str(datetime.datetime.now())[:10]
 
 
 @app.route('/')
 def status():
-    # return render_template('index.html')
-    date = '2018-09-14'
-    items = len(db.get(nowstr))
-    return 'Hello,we found '+ str(items)
+    return 'Hello,we found '+ 42
 
 @app.route('/events')
 @app.route('/events/<string:date>')
